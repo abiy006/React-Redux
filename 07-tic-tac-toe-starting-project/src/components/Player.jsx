cd7deb9440569e71e0855f8a90beb257c247712b
Abiy <AbiyHussen@gmail.com>
2024-02-02 16:28:46 +0300

    Two way biniding for input filed and text is done

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,27 +1,33 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
-
+export default function Player({ initialName, symbol }) {
+const [palyerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(true);
+        // setIsEditing(isEditing ? false : true); //not recommended
+        // setIsEditing(!isEditing);// not recommended
+        setIsEditing((editing) => !editing)
     }
-    console.log("Player is running ...")
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
 
-    let playerName = <span className="player-name">{name}</span>;
+    // let buttonCaption = 'Edit';
+    let editablePlayerName = <span className="player-name">{palyerName}</span>;
 
-    if (isEditing) { 
-        playerName = <input type="text" required />;
+    if (isEditing) {
+        // buttonCaption = "Save";
+        editablePlayerName = <input type="text" required value={palyerName} onChange={handleChange} />;
      }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
 
     );
