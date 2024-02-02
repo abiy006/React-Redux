d529f79df8d6784319596e6b4710cffc5dd7e6e7
Abiy <AbiyHussen@gmail.com>
2024-02-02 14:19:31 +0300

    Default props value is set to menu

--- 02-starting-project/src/components/Tabs.jsx
+++ 02-starting-project/src/components/Tabs.jsx
@@ -1,5 +1,7 @@
-export default function Tabs({ children, buttons, ButtonsContainer }) {
+//import the custom Section value first, if you want to use it 
+// export default function Tabs({ children, buttons, ButtonsContainer = Section }) {
 
+export default function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
     // const ButtonsContainer = buttonsContainer;
     return (
         <>
