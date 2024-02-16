cfa78370e387fe41f50fc145fcab5db28032beed
Abiy <AbiyHussen@gmail.com>
2024-02-16 10:57:46 +0300

    Lifting the state up

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
 const initialGameboard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquere, activePlayerSymbol }) {
+    const [gameBoad, setGameBoard] = useState(initialGameboard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            // prevGameBoard[rowIndex][colIndex] = "X";
+            // return prevGameBoard;
+
+            
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            // updatedBoard[rowIndex][colIndex] = "X";
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            
+            return updatedBoard;
+        });
+
+        onSelectSquere();
+    }
     return (
         <ol id="game-board">
-            {initialGameboard.map((row, rowIndex) => (
+            {gameBoad.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
