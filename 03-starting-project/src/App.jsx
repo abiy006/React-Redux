9d2d0298e6f2b35468bf8ec029b5c29104b44334
Abiy <AbiyHussen@gmail.com>
2024-02-21 16:26:43 +0300

    First commmit on project 03

--- 03-starting-project/src/App.jsx
+++ 03-starting-project/src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
+
+  const [userInputState, setUserInput] = useState({
+    initialInvestement: 10000,
+    annualInvestement: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInputState} onChangeInput={handleChange}/>
+      <Results input={userInputState}/>
     </>
   );
 }
