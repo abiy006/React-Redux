19067bcbebafcd3bd2e819837e8a8c1304caeca7
Abiy Hussen Aman <AbiyHussen@gmail.com>
2024-02-23 14:12:06 +0300

    Checking

---
+++ 05-starting-project/src/components/Player.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [enterPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  function handleChange(event) {
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick(){
+    setSubmitted(true);
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {submitted ? enterPlayerName : 'unknown entity'} </h2>
+      <p>
+        <input type="text" onChange={handleChange} value={enterPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
