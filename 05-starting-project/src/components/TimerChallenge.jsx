bc514aa397b0f148e2ae853a0052e50ac340c433
Abiy <AbiyHussen@gmail.com>
2024-02-28 14:19:24 +0300

    Adding modal components

--- 05-starting-project/src/components/TimerChallenge.jsx
+++ 05-starting-project/src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
@@ -18,9 +19,11 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
+    <>
+    {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
     <section className="challenge">
       <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
+      {/* {timerExpired && <p>You lost!</p>} */}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
@@ -33,5 +36,6 @@ export default function TimerChallenge({ title, targetTime }) {
         {timerStarted ? "Time is running ..." : "Timer inactive"}
       </p>
     </section>
+    </>
   );
 }
