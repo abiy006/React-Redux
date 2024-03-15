d41f1c9650ced83544a89b04d9fecd95503cda39
Abiy <AbiyHussen@gmail.com>
2024-03-15 15:01:18 +0300

    Loading based on data fetching|

--- 06-connecting-to-database/src/components/AvailablePlaces.jsx
+++ 06-connecting-to-database/src/components/AvailablePlaces.jsx
@@ -2,22 +2,27 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setisFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      setisFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setisFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data ..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
