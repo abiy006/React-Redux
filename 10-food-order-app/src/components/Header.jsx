433caaed0146e342d3c915d9219063b6d2b6ed06
Abiy <AbiyHussen@gmail.com>
2024-03-21 11:05:49 +0300

    Context and Reducer is completed|

--- 10-food-order-app/src/components/Header.jsx
+++ 10-food-order-app/src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItmes = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +16,7 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly={true}>Cart (0) </Button>
+        <Button textOnly={true}>Cart ({totalCartItmes}) </Button>
       </nav>
     </header>
   );
--- 10-food-order-app/src/store/CartContext.jsx
+++ 10-food-order-app/src/store/CartContext.jsx
@@ -1,14 +1,82 @@
-import { createContext } from "react";
+import { startTransition } from "react";
+import { createContext, useReducer } from "react";
 
 const CartContext = createContext({
-    //used for better autocompletions
-    items: [],
-    addItem: (item) => {},
-    removeItem: (id) => {}
+  //used for better autocompletions
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
 });
 
-export function CartContextProvider({ children}) {
-    return <CartContext.Provider>{children}</CartContext.Provider>
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    //UPDATE THE STATE TO ADD ITEM
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    //UPDATE THE STATE TO REMOVE ITEM
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  const [ cart, dispatchCartAction ] = useReducer(cartReducer, { items: [] });
+
+    function addItem(item) {
+        dispatchCartAction({type: 'ADD_ITEM', item: item});
+    }
+
+    function removeItem(id) {
+        dispatchCartAction({type: 'REMOVE_ITEM', id});
+    }
+
+    const cartContext = {
+        items: cart.items,
+        addItem,//addItem: addItem
+        removeItem//removeItem: removeItem
+    }
+
+  return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>;
 }
 
 export default CartContext;
