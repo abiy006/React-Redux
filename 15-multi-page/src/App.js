0cc7918b3216acf849abee379ee5a48f2b13a691
Abiy <AbiyHussen@gmail.com>
2024-04-15 15:32:42 +0300

    Alternate ways of route definitions

--- 15-multi-page/src/App.js
+++ 15-multi-page/src/App.js
@@ -1,13 +1,30 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  RouterProvider,
+  // Route
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
 const routerX = createBrowserRouter([
-  { path: '/', element: <HomePage />  },
-  { path: '/products', element: <ProductsPage />  },
-])
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
+  // return <RouterProvider router={router} />;
   return <RouterProvider router={routerX} />;
+
 }
 
 export default App;
