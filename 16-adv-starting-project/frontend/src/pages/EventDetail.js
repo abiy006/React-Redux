5d858b5091bb007c08b3e7ff4b62e3eedceb6120
Abiy Hussen Aman <AbiyHussen@gmail.com>
2024-05-03 14:36:02 +0300

    it's completed now

--- 16-adv-starting-project/frontend/src/pages/EventDetail.js
+++ 16-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 function EventDetailPage() {
+  const params = useParams();
     return (
       <>
             <h1>EventDetailPage</h1>
+            <p>Event ID: {params.eventIdX}</p>
       </>
     );
   
--- 16-adv-starting-project/frontend/src/pages/Events.js
+++ 16-adv-starting-project/frontend/src/pages/Events.js
@@ -1,9 +1,33 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "some event",
+  },
+  {
+    id: "e2",
+    title: "another event",
+  },
+  {
+    id: "e3",
+    title: "another event 2",
+  },
+];
+
 function EventsPage() {
-    return (
-      <>
-          <h1>EventsPage</h1>
-      </>
-    );
-  
-  }
-  export default EventsPage;
+  return (
+    <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            {/* <Link to={`/events/${event.id}`}></Link> */}
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default EventsPage;
