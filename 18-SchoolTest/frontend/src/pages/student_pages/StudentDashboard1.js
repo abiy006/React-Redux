7a3708095f8568bd76416e4e03deca1a9c59c0eb
Abiy Hussen Aman <AbiyHussen@gmail.com>
2024-05-30 01:26:13 +0300

    Attendance is modified

--- 18-SchoolTest/frontend/src/pages/student_pages/StudentDashboard1.js
+++ 18-SchoolTest/frontend/src/pages/student_pages/StudentDashboard1.js
@@ -32,40 +32,317 @@ const DUMMY_EVENTS = [
   },
 ];
 
-
-
-// function x(xx) {
-//   return <li key={xx} className={classes.weekNames}>
-//      <button>{xx}</button>
-//   </li>
-// }
-
-// function handleDaysInMonth(arrayLength) {
-//    for (let index = 1; index < arrayLength; index++) {
-//     x(arrayLength)
-//    }
-// }
+// const DUMMY_ATTENDANCE_WEEK_NAME = [
+//   {
+//     id: "e1",
+//     month_date_no: "1",
+//     month_date_img:
+//       "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+//     month_date_color: "2024-05-23",
+//     month_date_decriptions: "Grade 6",
+//   },
+//   {
+//     id: "e2",
+//     month_date_no: "2",
+//     month_date_img:
+//       "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+//     month_date_color: "2024-05-23",
+//     month_date_decriptions: "Grade 6",
+//   },
+//   {
+//     id: "e3",
+//     month_date_no: "3",
+//     month_date_img:
+//       "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+//     month_date_color: "2024-05-23",
+//     month_date_decriptions: "Grade 6",
+//   },
+//   {
+//     id: "e4",
+//     month_date_no: "4",
+//     month_date_img:
+//       "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+//     month_date_color: "2024-05-23",
+//     month_date_decriptions: "Grade 6",
+//   },
+//   {
+//     id: "e5",
+//     month_date_no: "5",
+//     month_date_img:
+//       "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+//     month_date_color: "2024-05-23",
+//     month_date_decriptions: "Grade 6",
+//   },
+//   {
+//     id: "e6",
+//     month_date_no: "6",
+//     month_date_img:
+//       "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+//     month_date_color: "2024-05-23",
+//     month_date_decriptions: "Grade 6",
+//   },
+//   {
+//     id: "e7",
+//     month_date_no: "7",
+//     month_date_img:
+//       "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+//     month_date_color: "2024-05-23",
+//     month_date_decriptions: "Grade 6",
+//   },
+// ];
+const DUMMY_ATTENDANCE_WEEK1 = [
+  {
+    id: "e1",
+    month_date_no: "1",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e2",
+    month_date_no: "2",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e3",
+    month_date_no: "3",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e4",
+    month_date_no: "4",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e5",
+    month_date_no: "5",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e6",
+    month_date_no: "6",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e7",
+    month_date_no: "7",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+];
+const DUMMY_ATTENDANCE_WEEK2 = [
+  {
+    id: "e1",
+    month_date_no: "1",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e2",
+    month_date_no: "2",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e3",
+    month_date_no: "3",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e4",
+    month_date_no: "4",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e5",
+    month_date_no: "5",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e6",
+    month_date_no: "6",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e7",
+    month_date_no: "7",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+];
+const DUMMY_ATTENDANCE_WEEK3 = [
+  {
+    id: "e1",
+    month_date_no: "1",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e2",
+    month_date_no: "2",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e3",
+    month_date_no: "3",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e4",
+    month_date_no: "4",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e5",
+    month_date_no: "5",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e6",
+    month_date_no: "6",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e7",
+    month_date_no: "7",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+];
+const DUMMY_ATTENDANCE_WEEK4 = [
+  {
+    id: "e1",
+    month_date_no: "1",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e2",
+    month_date_no: "2",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e3",
+    month_date_no: "3",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e4",
+    month_date_no: "4",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e5",
+    month_date_no: "5",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e6",
+    month_date_no: "6",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+  {
+    id: "e7",
+    month_date_no: "7",
+    month_date_img:
+      "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1o4msJ.img?w=800&h=435&q=60&m=2&f=jpg",
+    month_date_color: "2024-05-23",
+    month_date_decriptions: "Grade 6",
+  },
+];
 
 const dateObject = new Date();
 function daysInMonth(month, year) {
   switch (month) {
-      case 1: // January
-      case 3: // March
-      case 5: // May
-      case 7: // July
-      case 8: // August
-      case 10: // October
-      case 12: // December
-          return 31;
-      case 4: // April
-      case 6: // June
-      case 9: // September
-      case 11: // November
-          return 30;
-      case 2: // February
-          return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) ? 29 : 28;
-      default:
-          return -1; // Invalid month
+    case 1: // January
+    case 3: // March
+    case 5: // May
+    case 7: // July
+    case 8: // August
+    case 10: // October
+    case 12: // December
+      return 31;
+    case 4: // April
+    case 6: // June
+    case 9: // September
+    case 11: // November
+      return 30;
+    case 2: // February
+      return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) ? 29 : 28;
+    default:
+      return -1; // Invalid month
   }
 }
 
@@ -73,28 +350,59 @@ const monthDays = dateObject.getMonth() + 1;
 const year = dateObject.getFullYear();
 
 
-const arrOfDays = new Array(daysInMonth(monthDays, year));
-   for (let index = 1; index < arrOfDays.length; index++) {
-    arrOfDays.push(index);
-   }
-// let dayNumber = 1;
-// Example usage:
-// let month = 1;
-// let year = 2024;
-// console.log("Number of days in " + monthDays + "th month of the year " + year + " is " + daysInMonth(monthDays, year));
-console.log("Number of days in " + monthDays + "th month of the year " + year + " is " + arrOfDays.length);
-console.log("arrOfDays - " + arrOfDays);
+console.log(
+  "Number of days in " +
+    monthDays +
+    "th month of the year " +
+    year +
+    " is " +
+    daysInMonth(monthDays, year)
+);
 
 function StudentDashboard1() {
   // const token = true;
   const date = new Date();
-  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   const currentDayOfWeek = daysOfWeek[date.getDay()];
-  const daysOfWeek2 = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const daysOfWeek2 = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const currentDayOfWeek2 = daysOfWeek2[date.getDay()];
-  const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const monthsOfYear = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   const currentMonthOfYear = monthsOfYear[date.getMonth()];
-  const monthsOfYear2 = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const monthsOfYear2 = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
   const currentMonthOfYear2 = monthsOfYear2[date.getMonth()];
   const currentYear = date.getFullYear();
 
@@ -105,9 +413,13 @@ function StudentDashboard1() {
 
   const currentTime = date.toLocaleTimeString();
   console.log(`date variable is - ${date} + datesOfMonthate is `);
-  console.log(`Today is ${currentDayOfWeek} :  short is - ${currentDayOfWeek2}   and the time is ${currentTime} and the month is ${currentMonthOfYear} : short -  ${currentMonthOfYear2} and the year is ${currentYear}`);
-  console.log(`Hour is ${currentHour} and the minute is ${currentMinute} and the second is ${currentSecond} and the millisecond is ${currentMicrosecond}`);
-  
+  console.log(
+    `Today is ${currentDayOfWeek} :  short is - ${currentDayOfWeek2}   and the time is ${currentTime} and the month is ${currentMonthOfYear} : short -  ${currentMonthOfYear2} and the year is ${currentYear}`
+  );
+  console.log(
+    `Hour is ${currentHour} and the minute is ${currentMinute} and the second is ${currentSecond} and the millisecond is ${currentMicrosecond}`
+  );
+
   // const x = daysInMonth(monthDays, year)
 
   return (
@@ -115,7 +427,7 @@ function StudentDashboard1() {
       <h1 className={classes.dashboard}>Events dashboard</h1>
       <article className={classes.event}>
         <div className={classes.studentRapper}>
-          <h1>Events event name</h1>
+          <h1>Events event name 1</h1>
           {/* <h1>{DUMMY_EVENTS[1].image}</h1> */}
           <Link>
             <div className={classes.imgDiv}>
@@ -132,58 +444,54 @@ function StudentDashboard1() {
             <button>Subjects</button>
           </div>
         </div>
+
         <div className={classes.studentRapper}>
-          <h1>Attendance</h1>
-          <h2>{currentMonthOfYear} - {currentYear}</h2>
+          <h1>Events event name 2</h1>
           <ul className={classes.weekNames_ul}>
-            {daysOfWeek2.map((element) => (
-              <li key={element} className={classes.weekNames}>
-                <p>{element}</p>
+            {daysOfWeek2.map((event) => (
+              <li key={event} className={classes.weekNames_li}>
+                <Link>
+                    <p>{event}</p>
+                </Link>
               </li>
             ))}
-            </ul>
-            <ul className={classes.list}>
-            {arrOfDays.map((element) => (
-              <li key={element} className={classes.list}>
-               <button>{element}</button>
+          </ul>
+          <ul className={classes.weekNames_ul}>
+            {DUMMY_ATTENDANCE_WEEK1.map((event) => (
+              <li key={event.id} className={classes.weekNames_li}>
+                <Link>
+                    <p>{event.month_date_no}</p>
+                </Link>
               </li>
             ))}
-            
-            </ul>
-{/* daysInMonth(monthDays, year) */}
-            
-
-          {/* <h1>{DUMMY_EVENTS[1].image}</h1> */}
-          <Link>
-            <div className={classes.imgDiv}>
-              <img src={DUMMY_EVENTS[1].image} alt={DUMMY_EVENTS[1].title} />
-              <time>{date.toString()}</time>
-            </div>
-          </Link>
-          <h1 className={classes.headerFont}>Event Full Name</h1>
-          <div className={classes.gradeSectionDiv}>
-            <h2>{DUMMY_EVENTS[1].grade}</h2>
-            <h2>{DUMMY_EVENTS[1].section}</h2>
-          </div>
-        </div>
-
-        <div className={classes.studentRapper}>
-          <h1>Events event name</h1>
-          {/* <h1>{DUMMY_EVENTS[1].image}</h1> */}
-          <Link>
-            <div className={classes.imgDiv}>
-              <img src={DUMMY_EVENTS[1].image} alt={DUMMY_EVENTS[1].title} />
-              <time>{date.toString()}</time>
-            </div>
-          </Link>
-          <h1 className={classes.headerFont}>Event Full Name</h1>
-          <div className={classes.gradeSectionDiv}>
-            <h2>{DUMMY_EVENTS[1].grade}</h2>
-            <h2>{DUMMY_EVENTS[1].section}</h2>
-          </div>
-          <div className={classes.studentButton}>
-            <button>Subjects</button>
-          </div>
+          </ul>
+          <ul className={classes.weekNames_ul}>
+            {DUMMY_ATTENDANCE_WEEK2.map((event) => (
+              <li key={event.id} className={classes.weekNames_li}>
+                <Link>
+                    <p>{event.month_date_no}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className={classes.weekNames_ul}>
+            {DUMMY_ATTENDANCE_WEEK3.map((event) => (
+              <li key={event.id} className={classes.weekNames_li}>
+                <Link>
+                    <p>{event.month_date_no}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className={classes.weekNames_ul}>
+            {DUMMY_ATTENDANCE_WEEK4.map((event) => (
+              <li key={event.id} className={classes.weekNames_li}>
+                <Link>
+                    <p>{event.month_date_no}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
 
         <div className={classes.studentRapper}>
