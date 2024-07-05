985e1da5f221c7349823a1d368a7b9597c106c1d
Abiy Hussen Aman <AbiyHussen@gmail.com>
2024-07-05 22:41:34 +0300

    Student detail page is under constraction

--- 18-SchoolTest/frontend/src/TCRUD/navigations/StudDashPartB.js
+++ 18-SchoolTest/frontend/src/TCRUD/navigations/StudDashPartB.js
@@ -12,9 +12,10 @@ const StudDashPartB = ({ tcruds }) => {
         <li key={tcrud.id} className={classes.item}>
           <NavLink to={`/s-crudX/${tcrud.number}`}>
             <img src={tcrud.image} alt={tcrud.title} />
-            <div className={classes.content} style={index % 2 === 0 ? {backgroundColor: "#1f1d1b"} : {backgroundColor: '#1f1d1b'}}>
-              <p>{tcrud.title}</p>
-              <time>{tcrud.date}</time>
+            {/* <div className={classes.content} style={index % 2 === 0 ? {backgroundColor: "#1f1d1b"} : {backgroundColor: '#1f1d1b'}}> */}
+            <div className={classes.content}>
+              <h3>{tcrud.title}</h3>
+              <p>{tcrud.date}</p>
               <p>{tcrud.number}</p>
             </div>
           </NavLink>
